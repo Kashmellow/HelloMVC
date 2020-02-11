11bc5fb7f382e665462b1630f34a032e7d5f0e0c
Kashmellow <ejodlowsky@pm.me>
2020-02-11 14:13:07 -0500

    did it

---
+++ HelloMVC/Controllers/GreetController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloMVC.Models;
+
+namespace HelloMVC.Controllers
+{
+    public class GreetController:Controller
+    {
+        public ViewResult Index(string name)
+        {
+        var model = new GreetModel();
+        model.Name = name;
+        return View(model);
+        //passing model to the view
+        }
+    }
+}
